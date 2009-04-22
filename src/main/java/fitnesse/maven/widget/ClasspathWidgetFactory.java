23508040a3989e914d1bfcf2c78263e6db3eea47
Dan Dudley <born2snipe@gmail.com>
2009-04-22 18:59:53 -0500

    changed to add a br after each classpath widget

--- src/main/java/fitnesse/maven/widget/ClasspathWidgetFactory.java
+++ src/main/java/fitnesse/maven/widget/ClasspathWidgetFactory.java
@@ -1,18 +1,20 @@
 /**
  * Copyright to the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at:
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is
  * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and limitations under the License.
  */
 package fitnesse.maven.widget;
 
+import fitnesse.html.HtmlUtil;
 import fitnesse.maven.util.StringUtil;
+import fitnesse.wikitext.WikiWidget;
 import fitnesse.wikitext.widgets.ClasspathWidget;
 import fitnesse.wikitext.widgets.ParentWidget;
 
@@ -23,10 +25,22 @@ import java.util.List;
 public class ClasspathWidgetFactory {
 
     public void build(ParentWidget parentWidget, List<String> dependencies) throws Exception {
-        new ClasspathWidget(parentWidget, String.format("%s %s", "!path", StringUtil.join(File.pathSeparator, dependencies)));
+        build(parentWidget, StringUtil.join(File.pathSeparator, dependencies));
     }
 
     public void build(ParentWidget parentWidget, String dependency) throws Exception {
         new ClasspathWidget(parentWidget, String.format("%s %s", "!path", dependency));
+        new BreakWidget(parentWidget);
+
+    }
+
+    private static class BreakWidget extends WikiWidget {
+        protected BreakWidget(ParentWidget parentWidget) {
+            super(parentWidget);
+        }
+
+        public String render() throws Exception {
+            return HtmlUtil.BRtag;
+        }
     }
 }
