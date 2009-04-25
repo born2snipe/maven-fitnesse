386a976af901c0456f69753aba74e69e07a40d04
Dan Dudley <born2snipe@gmail.com>
2009-04-25 13:57:10 -0500

    created a local repo resolver

---
+++ src/main/java/fitnesse/maven/util/LocalRepoResolver.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright to the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+package fitnesse.maven.util;
+
+import fitnesse.maven.PomFile;
+
+import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class LocalRepoResolver {
+    private static final Pattern LOCAL_REPO_PATTERN = Pattern.compile("<localRepository>(.+)</localRepository>");
+    private CommandShell shell;
+
+    public LocalRepoResolver(CommandShell shell) {
+        this.shell = shell;
+    }
+
+    public File resolve(PomFile pomFile) {
+        String consoleOutput = shell.execute(pomFile.getDirectory(), mvnCommand(), "help:effective-settings");
+        Matcher matcher = LOCAL_REPO_PATTERN.matcher(consoleOutput);
+        matcher.find();
+        return new File(matcher.group(1));
+    }
+
+    protected String mvnCommand() {
+        if (isWindows()) {
+            return "mvn.bat";
+        }
+        return "mvn";
+    }
+
+    protected boolean isWindows() {
+        return System.getProperty("os.name").toLowerCase().contains("windows");
+    }
+}
